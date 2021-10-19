102aebee192bbd9689e5704be064f274d3ed96f8
Pavel Kaganer <pashakag@gmail.com>
2021-10-19 19:01:49 +0500

    Test 4 check

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 import prompt
+
+
 def welcome_user():
-	name = prompt.string("May I have your name? ")
-	print(f"Hello, {name}!")
+    name = prompt.string("May I have your name? ")
+    print(f"Hello, {name}!")
+
+
 def main():
-	welcome_user()
+    welcome_user()
+
+
 if __name__ == "__main__":
-	main()
+    main()
