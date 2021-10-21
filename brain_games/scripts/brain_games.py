c4ddba45a77fa16f9e165b4d9ba4bef4303f59e2
Pavel Kaganer <pashakag@gmail.com>
2021-10-21 12:51:35 +0500

    repair

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games import cli
+from brain_games.cli import welcome_user
 
 
 def main():
     print("Welcome to the Brain Games!")
-    cli.welcome_user()
+    welcome_user()
 
 
 if __name__ == "__main__":
